217882f607ff203e4487820cac07726bd2fdacc4
leandrocs92 <39960527+leandrocs92@users.noreply.github.com>
2018-07-06 20:06:19 -0300

    vendas DAO
    implementação de vendas DAO

--- JLMANAGEMENT/src/DAO/VendasDAO.java
+++ JLMANAGEMENT/src/DAO/VendasDAO.java
@@ -4,6 +4,7 @@ package DAO;
 import ferramentas.FabricaConexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import model.Produto;
 
 
@@ -18,6 +19,25 @@ public class VendasDAO {
             consultaInsert.setDouble(4, Double.parseDouble(info[0]));
             consultaInsert.setDouble(5, Double.parseDouble(info[3]));
             consultaInsert.execute();
+        } catch (Exception ex) {
+            System.err.println("Erro de execução da SQL..");
+        }
+    }
+    public static Vendas buscaVenda(String nome, String ano){
+        try (Connection con = FabricaConexao.criaConexao()) {
+            String sqlVenda = "select * from vendas where nomemercadoria = ? and ano = ?";
+            PreparedStatement consultaInsert = con.prepareStatement(sqlVenda);
+            consultaInsert.setString(1, nome);
+            consultaInsert.setString(2, ano);
+            ResultSet resultado = consultaInsert.executeQuery();
+            while(resultado.next()){
+                String nomeMercadoria = resultado.getString("nomemercadoria");
+                String mes = resultado.getString("mes");
+                String anoMercadoria = resultado.getString("ano");
+                double valor = resultado.getDouble("valorvendido");
+                int id = resultado.getInt("idvendas");
+                
+            }
         } catch (Exception ex) {
             System.err.println("Erro de execução da SQL..");
         }
