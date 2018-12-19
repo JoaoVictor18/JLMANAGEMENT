72441eb26e05b39454fc91c3fee6adc44aab2360
JoaoVictor18 <39972047+JoaoVictor18@users.noreply.github.com>
2018-12-18 23:02:51 -0200

    varias alterações
    varias alterações

---
+++ JLMANAGEMENT/JLMANAGEMENT HTML/JS/script.js
@@ -0,0 +1,220 @@
+var pagina = document.location.href;
+var vet = pagina.split("/");
+console.log(vet[vet.length-1]);
+//pegar a tela atual
+
+var vetorResposta;
+var contaProduto;
+var objSelecionado;
+
+function buscaProdutoAtualiza(){
+    con.open("GET", "http://localhost:8080/JLMANAGEMENT/MercadoriaServlet?serv=&",
+    true);
+
+    //formato das  informações
+    con.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    con.onload = function(x) {
+        var respostaServ = x.currentTarget.responseText;
+        alert(respostaServ);
+        };
+
+    // verificar como faz pra pegar o name do form
+    
+       /* var nome = document.forms["form1AtualizaMerc"]["nome"].value;
+        var nome2 = 
+        var mVenda = document.forms["form1AtualizaMerc"]["mesvenda"].value;
+        var aVenda = document.forms["form1AtualizaMerc"]["anovenda"].value;
+        var vVenda = document.forms["form1AtualizaMerc"]["valorvenda"].value;*/
+}
+
+function atualizaProduto(){
+    //outra função? tirar dúvida. como fazer para pegar a mercadoria selecionada no quadro
+    var nome = document.forms["form1AtualizaMerc"]["nome"].value;
+    //se a opção desejada está ativa
+    var ativo = document.forms["form1AtualizaMerc"]["compra"].value;
+    if( ativo == true){
+        var qCompra = document.forms["form1AtualizaMerc"]["quantcompra"].value;
+        var dCompra = document.forms["form1AtualizaMerc"]["datacompra"].value;
+        con.send("serv=atualiza&nome=" +nome+ "&quantcompra="+qCompra+ "&datacompra="+dCompra+ "&ativo="+ativo);
+    }
+    var con = new XMLHttpRequest();
+    //formato das  informações
+    con.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    con.open("post", "http://localhost:8080/JLMANAGEMENT/MercadoriaServlet",
+    true);
+    con.onload = function(x) {
+        var respostaServ = x.currentTarget.responseText;
+        alert(respostaServ);
+        };
+    /*else{
+        con.send("serv=consulta&nome=" +nome);
+    }*/
+}
+function exibicao(){
+    var form = document.forms["formEx"];
+    form["nome"].value = objSelecionado.nome;
+    form["tipo"].value = objSelecionado.tipo;
+    form["fornec"].value = objSelecionado.fornecedor;
+    form["precM"].value = objSelecionado.custo;
+    form["imp"].value = objSelecionado.percentualImposto;
+    form["perFrete"].value = objSelecionado.percentualFrete;
+    form["date"].value = objSelecionado.dataCompra;
+    form["inf"].value = objSelecionado.infAdicionais;
+    form["quantC"].value = objSelecionado.qntCompra;
+    form["quantE"].value = objSelecionado.qntEstoque;
+    form["quantM"].value = objSelecionado.qntMim;
+    form["ref"].value = objSelecionado.referencia;
+    //objAtivo = objSelecionado;
+}
+function selecionar(id){
+    var objSelecionado = vetorResposta.resposta[id];
+    var lista = document.getElementById("listaProd").children;
+    lista[id].classList.toggle("selecionado");
+ }
+function buscaProduto(){
+   var requisicao = "http://localhost:8080/JLMANAGEMENT/MercadoriaServlet?";
+   //verifica em qual página o usuário está
+   var ativo = document.forms["formConsulta"]["infadd"].value;
+    var pagina = document.location.href;
+        var vet = pagina.split("/");
+        var atual = (vet[vet.length-1]);
+        if(atual == "consultaMerc.html"){
+            //pega as informações dos inputs
+            var nome = document.forms["formConsulta"]["nome"].value;
+            if(ativo == true){
+                var tipo = document.forms["formConsulta"]["tipo"].value;
+                var preco = document.forms["formConsulta"]["preco"].value;
+                var fornec = document.forms["formConsulta"]["fornecedor"].value;
+                requisicao+="serv=consulta&nome=" +nome+ "&tipo="+tipo+ "&preco="+preco+ "&fornecedor="+fornec+ "&ativo="+ ativo;
+            }else{
+                requisicao+="serv=consulta&nome=" +nome+ "ativo="+ativo;
+            }
+        }else{
+            if(atual == "atualizaMerc.html"){
+                var nome = document.forms["form1AtualizaMerc"]["nome"].value;
+                requsicao+="serv=atualiza&nome="+nome;
+            }
+        }    
+        //estrutura
+        var con = new XMLHttpRequest();
+        //formato das  informações
+        con.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        con.open("GET", requisicao,
+        true);
+    //tratar o retorno dos servlets
+    if(atual == "consultaMerc.html"){
+        con.onload = function(x) {
+            var respostaServ = x.currentTarget.responseText;
+            vetorResposta = JSON.parse(respostaServ);
+            if (vetorResposta.resultado == "Mercadoria não encontrada!" && vetorResposta.cod == 200 ) {
+                alert("Não foi possível encontrar este produto!");
+            }else{
+                //como armazenar o retorno do vetor de mercadorias encontradas
+                if(vetorResposta.cod == 200 && vetorResposta.resposta.length > 0){
+                    var lista = document.getElementById("listaProd");
+                    for(i=0;i<vetorResposta.length;i++){
+                        var novoElemento = document.createElement("li");
+                        novoElemento.setAttribute("onclick","selecionar("+contaProduto+")");
+                        var texto = document.createTextNode(vetorResposta.resultado[i].nome);
+                        novoElemento.appendChild(texto);
+                        lista.appendChild(novoElemento);
+                        contaProduto++;
+                    }   
+                }else{
+                    if(vetorResposta.cod == 302){
+                        alert(vetorResposta.resposta);
+                    }
+                }
+            }
+        };
+    }else{
+        if(atual == "atualizaMerc.html"){
+            con.onload = function(x) {
+                var respostaServ = x.currentTarget.responseText;
+                var objResposta = JSON.parse(respostaServ);
+                if (objResposta.resultado == null) {
+                    alert("Não foi possível encontrar este produto!");
+                }else{
+    
+                }
+            /*alert(respostaServ);*/
+            };
+        }
+    }    
+  
+}
+
+//copiei a função só para facilitar
+
+/*function addLista(){
+    var lista = document.getElementById("listaProd");
+    
+    var nomeProd = document.getElementById("nomeProduto");
+    
+    var novoElemento = document.createElement("li");
+    
+    var texto = document.createTextNode(nomeProd.value);
+    novoElemento.setAttribute("onclick","selecionar("+contaProduto+")");
+    
+    contaProduto++;
+    
+    novoElemento.appendChild(texto);
+    
+    lista.appendChild(novoElemento);
+    
+    nomeProd.value = "";
+}*/
+function limpaCampos(){
+    var formCadastra = document.forms["formCadMerc"];
+    formCadastra["nome"].value = "";
+    formCadastra["tipo"].value = "";
+    formCadastra["fabricante"].value = "";
+    formCadastra["precocusto"].value = "";
+    formCadastra["imposto"].value = "";
+    formCadastra["frete"].value = "";
+    formCadastra["data"].value = "";
+    formCadastra["quantcompra"].value = "";
+    formCadastra["infadd"].value = "";
+    formCadastra["quantestoque"].value = "";
+    formCadastra["qntminima"].value = "";
+    formCadastra["referencia"].value = "";
+}
+function cadastraMerc(){
+    var nome = document.forms["formCadMerc"]["nome"].value;
+    var tipoMerc = document.forms["formCadMerc"]["tipo"].value;
+    var fabricante = document.forms["formCadMerc"]["fabricante"].value;
+    var pCusto = document.forms["formCadMerc"]["precocusto"].value;
+    var imposto = document.forms["formCadMerc"]["imposto"].value;
+    var frete = document.forms["formCadMerc"]["frete"].value;
+    var data = document.forms["formCadMerc"]["data"].value;
+    var qCompra = document.forms["formCadMerc"]["quantcompra"].value;
+    var qEstoque = document.forms["formCadMerc"]["quantestoque"].value;
+    var qMinEstoque = document.forms["formCadMerc"]["qntmin"].value;
+    var referencia = document.forms["formCadMerc"]["referencia"].value;
+    if(document.forms["formCadMerc"]["checkinf"].value == true){
+        var infAdi = document.forms["formCadMerc"]["infadd"].value;
+        con.send("serv=cadastra&nome=" + nome + "&tipo=" + tipoMerc + "&fabricante=" + fabricante +
+            "&precocusto=" + pCusto+ "&imposto=" + imposto + "&frete=" + frete +
+            "&data="+data+ "&quantcompra"+ qCompra+ "&infadd"+infAdi+ 
+            "&quantestoque"+qEstoque+ "&qntmin"+qMinEstoque+ "&referencia"+referencia );
+    }else{
+        con.send("serv=cadastra&nome=" + nome + "&tipo=" + tipoMerc + "&fabricante=" + fabricante +
+            "&precocusto=" + pCusto+ "&imposto=" + imposto + "&frete=" + frete +"&data="+data+ "&quantcompra="
+            +qCompra+"&quantestoque="+qEstoque+ "&qntmin="+qMinEstoque+ "&referencia="+referencia );
+    }
+    var con = new XMLHttpRequest();
+    con.open("post", "http://localhost:8080/JLMANAGEMENT/MercadoriaServlet",
+    true);
+
+    //formato das  informações
+    con.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    con.onload = function(x) {
+        var respostaServ = x.currentTarget.responseText;
+        alert(respostaServ);
+        //fazer verificação
+        limpaCampos();
+    };
+}
+    
--- JLMANAGEMENT_SERVLETS/JLMANAGEMENT/src/java/Servlets/MercadoriaServlet.java
+++ JLMANAGEMENT_SERVLETS/JLMANAGEMENT/src/java/Servlets/MercadoriaServlet.java
@@ -19,29 +19,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Produto;
 
-
 @WebServlet(name = "MercadoriaServlet", urlPatterns = {"/MercadoriaServlet"})
 public class MercadoriaServlet extends HttpServlet {
 
-   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         Configuracao.configResponseRequest(request, response);
         try (PrintWriter out = response.getWriter()) {
             String servico = request.getParameter("serv");
-            
-            switch(servico){
-                case "cadastra":{
+
+            switch (servico) {
+                case "cadastra": {
                     cadastraMerc(request, out);
-                }break;
-                case "atualiza":{
+                }
+                break;
+                case "atualiza": {
                     //chamar metodo busca
                     atualizaMerc(request, out);
-                }break;
-                case "consulta":{
+                }
+                break;
+                case "consulta": {
                     consultaMerc(request, out);
                 }
-                default:{
+                case "exibicao": {
+                    exibicaoMerc(request, out);
+                }
+                default: {
                     Resposta resultado = new Resposta(404, "O serviço requisitado não existe.(cadastra, buscaMercadoria)");
                     out.print(resultado.toJSON());
                 }
@@ -49,21 +52,18 @@ public class MercadoriaServlet extends HttpServlet {
         }
     }
 
-   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-   
     @Override
     public String getServletInfo() {
         return "Short description";
@@ -83,53 +83,53 @@ public class MercadoriaServlet extends HttpServlet {
         String qntEstoque = request.getParameter("qntestoque");
         String qntMinima = request.getParameter("qntminima");
         String referenia = request.getParameter("referencia");
-        
-        if(nome == null || tipoMerc == null || fabricante == null || precoCus == null 
-              || perImposto == null || perFrete == null || dataCompra == null ||
-                quantCompra == null || infAdd == null || qntEstoque == null ||
-                qntMinima == null || referenia == null){
+
+        if (nome == null || tipoMerc == null || fabricante == null || precoCus == null
+                || perImposto == null || perFrete == null || dataCompra == null
+                || quantCompra == null || infAdd == null || qntEstoque == null
+                || qntMinima == null || referenia == null) {
             Resposta resultado = new Resposta(301, "Os parâmetros (nome, tipoMerc, fabricante, precoCus,perImposto,"
                     + "perFrete,dataCompra,infAdd,qntEstoque,qntMinima,referencia) devem ser informados");
             out.print(resultado.toJSON());
-        }else{
-            try{
+        } else {
+            try {
                 double precoCusto = Double.parseDouble(precoCus);
                 double perImp = Double.parseDouble(perImposto);
                 double percentFrete = Double.parseDouble(perFrete);
                 double qntCompra = Double.parseDouble(quantCompra);
                 double quantEstoque = Double.parseDouble(qntEstoque);
                 double quantMinima = Double.parseDouble(qntMinima);
-                String []data = dataCompra.split("/");
+                String[] data = dataCompra.split("/");
                 int dia = Integer.parseInt(data[0]);
                 int mes = Integer.parseInt(data[1]);
                 int ano = Integer.parseInt(data[2]);
                 Calendar novoCalendar = Calendar.getInstance();
-                    novoCalendar.set(Calendar.DAY_OF_MONTH, dia);
-                    novoCalendar.set(Calendar.MONTH, mes);
-                    novoCalendar.set(Calendar.YEAR, ano);
-                    Date dateCompra = novoCalendar.getTime();
-                    
-                Produto novoProduto = new Produto(nome, tipoMerc, precoCus, infAdd, percentFrete, percentFrete, 
-                        quantEstoque, quantMinima, precoCusto, 
+                novoCalendar.set(Calendar.DAY_OF_MONTH, dia);
+                novoCalendar.set(Calendar.MONTH, mes);
+                novoCalendar.set(Calendar.YEAR, ano);
+                Date dateCompra = novoCalendar.getTime();
+
+                Produto novoProduto = new Produto(nome, tipoMerc, precoCus, infAdd, percentFrete, percentFrete,
+                        quantEstoque, quantMinima, precoCusto,
                         qntCompra, dateCompra, referenia);
                 //criar método cadastra no controller
                 //ProdutoController.cadastraProdudo(novoProduto);
                 boolean verificaRetorno = ProdutoController.buscaProdutoExistente(novoProduto);
-                if(verificaRetorno != false){
+                if (verificaRetorno != false) {
                     Resposta resultado = new Resposta(200, "Cadastro efetuado com sucesso.");
                     out.print(resultado.toJSON());
                     //mensagem "deseja efetuar um novo cadastro?"
-                }else{
-                    Resposta resultado = new Resposta(301, "Produto já cadastrado. Deseja atualizar a mercadoria?");
+                } else {
+                    Resposta resultado = new Resposta(200, "Produto já cadastrado.");
                     out.print(resultado.toJSON());
                     //limpar os campos, verificar como é feito
                 }
-            }catch(NumberFormatException ex){
-                Resposta resultado = new Resposta(302, "Os parâmetros (precoCusto, perImp,percentFrete,qntCompra"
-                        + "quantEstoque,qntMinima) não são números.");
+            } catch (NumberFormatException ex) {
+                Resposta resultado = new Resposta(302, "Os parâmetros (preco custo, percentual imposto,percentual frete,quantidade comprada"
+                        + "quantidade estoque,quantida minima) não são números.");
                 out.print(resultado.toJSON());
             }
-        }    
+        }
     }
 
     private void atualizaMerc(HttpServletRequest request, PrintWriter out) {
@@ -137,51 +137,57 @@ public class MercadoriaServlet extends HttpServlet {
         String nome = request.getParameter("nome");
         String quantidadeC = request.getParameter("quantcompra");
         String dataC = request.getParameter("datacompra");
-        String quantidadeV = request.getParameter("quantVendida");
+        String ativo = request.getParameter("ativo");
+        /*String quantidadeV = request.getParameter("quantvendida");
         String mesV = request.getParameter("mesvenda");
         String anoV = request.getParameter("anovenda");
-        String valorV = request.getParameter("valorvenda");
+        String valorV = request.getParameter("valorvenda");*/
         Vector<Produto> mercadorias = new Vector<>();
         String nomeSelecao = "";
-        if(nome == null){
+        if (nome == null) {
             Resposta resultado = new Resposta(302, "O parâmetro (nome) não pode ser nulo.");
             out.print(resultado.toJSON());
-        }else{
-            try{
-            mercadorias = ProdutoController.buscaProduto(nome);
-            //compra = true fazer if
-                if(quantidadeC == null || dataC == null){
-                    Resposta resultado = new Resposta(302, "Os parâmetros (nome, quantidadeC, dataC) não podem ser nulos.");
-                    out.print(resultado.toJSON());
-                }else{
-                    double quantCompra = Double.parseDouble(quantidadeC);
-                    String []data = dataC.split("/");
-                    int dia = Integer.parseInt(data[0]);
-                    int mes = Integer.parseInt(data[1]);
-                    int ano = Integer.parseInt(data[2]);
-                    Calendar novoCalendar = Calendar.getInstance();
+        } else {
+            try {
+                mercadorias = ProdutoController.buscaProduto(nome);
+                if (ativo.equals("true")) {
+                    if (quantidadeC == null || dataC == null) {
+                        Resposta resultado = new Resposta(302, "Os parâmetros (nome, quantidade, data compra) não podem ser nulos.");
+                        out.print(resultado.toJSON());
+                    } else {
+                        double quantCompra = Double.parseDouble(quantidadeC);
+                        String[] data = dataC.split("/");
+                        int dia = Integer.parseInt(data[0]);
+                        int mes = Integer.parseInt(data[1]);
+                        int ano = Integer.parseInt(data[2]);
+                        Calendar novoCalendar = Calendar.getInstance();
                         novoCalendar.set(Calendar.DAY_OF_MONTH, dia);
                         novoCalendar.set(Calendar.MONTH, mes);
                         novoCalendar.set(Calendar.YEAR, ano);
                         Date dateCompra = novoCalendar.getTime();
-                    
-                if(mercadorias.isEmpty()){
-                    Resposta resultado = new Resposta(302, "Mercadoria não encontrada!");
-                    out.print(resultado.toJSON());
-                }
-                //exibir na tela as opções encontradas
-                // apos selecionar o nome na tela de exibição atualizar a variável nomeSelecao
-                nomeSelecao = "";
-                for(int i = 0; i < mercadorias.size(); i++){
-                    if(mercadorias.get(i).getNome().equals(nomeSelecao)){
-                        mercadorias.get(i).setDataCompra(dateCompra);
-                        mercadorias.get(i).setQntCompra(quantCompra);
-                        ProdutoController.atualizaMercCompra(mercadorias.get(i));
+
+                        if (mercadorias.isEmpty()) {
+                            Resposta resultado = new Resposta(200, "Mercadoria não encontrada!");
+                            out.print(resultado.toJSON());
+                        } else {
+                            nomeSelecao = "";
+                            for (int i = 0; i < mercadorias.size(); i++) {
+                                if (mercadorias.get(i).getNome().equals(nomeSelecao)) {
+                                    mercadorias.get(i).setDataCompra(dateCompra);
+                                    mercadorias.get(i).setQntCompra(quantCompra);
+                                    ProdutoController.atualizaMercCompra(mercadorias.get(i));
+                                }
+                                Resposta resultado = new Resposta(200, "Mercadoria não encontrada!");
+                                out.print(resultado.toJSON());
+                                //exibir na tela as opções encontradas
+                                // apos selecionar o nome na tela de exibição atualizar a variável nomeSelecao
+
+                            }
+                        }
                     }
                 }
-            }
-            //código comentado será feito na venda
-            /*//outro if para venda = true
+                //código comentado será feito na venda
+                /*//outro if para venda = true
             //atualizar a variável nomeSelecao
             nomeSelecao = "";
             if(quantidadeV == null || mesV == null || anoV == null || valorV == null){
@@ -197,7 +203,7 @@ public class MercadoriaServlet extends HttpServlet {
                 }
                 
             }*/
-            }catch(NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 Resposta resultado = new Resposta(302, "O parâmetro (nome) informado não pode conter números ou carácteres especiais.");
                 out.print(resultado.toJSON());
             }
@@ -205,52 +211,57 @@ public class MercadoriaServlet extends HttpServlet {
     }
 
     private void consultaMerc(HttpServletRequest request, PrintWriter out) {
-       String nome = request.getParameter("nome");
-       String tipoM = request.getParameter("tipo");
-       String precoM = request.getParameter("preco");
-       String fornecedor = request.getParameter("fornecedor");
-       Vector<Produto> mercadorias = new Vector<>();
-       String nomeSelecao = "";
-       if(nome == null){
+        String nome = request.getParameter("nome");
+        String tipoM = request.getParameter("tipo");
+        String precoM = request.getParameter("preco");
+        String fornecedor = request.getParameter("fornecedor");
+        String ativo = request.getParameter("ativo");
+        Vector<Produto> mercadorias = new Vector<>();
+        //String nomeSelecao = "";
+        if (nome == null) {
             Resposta resultado = new Resposta(302, "O parâmetro (nome) não pode ser nulo!");
             out.print(resultado.toJSON());
-       }else{
-           try{
-                //filtros adicionais for == true
-                //chamada de método dentro do else
+        } else {
+            if (ativo.equals("false")) {
                 mercadorias = ProdutoController.buscaProduto(nome);
-                if(mercadorias.isEmpty()){
-                    Resposta resultado = new Resposta(302, "Mercadoria não encontrada!");
+                if (mercadorias.isEmpty()) {
+                    Resposta resultado = new Resposta(200, "Mercadoria não encontrada!");
                     out.print(resultado.toJSON());
                 }else{
-                    //exibir na tela web as mercadorias encontradas
+                    Resposta saida = new Resposta(200, mercadorias.toArray());
+                    out.print(saida.toJSON());
                 }
-                if(tipoM == null || precoM == null || fornecedor == null){
-                     Resposta resultado = new Resposta(302, "O parâmetros (tipoM,precoM e fornecedor) não podem ser nulos");
-                     out.print(resultado.toJSON());
-                }else{
-                    double preco  = Double.parseDouble(precoM);
-                    mercadorias = ProdutoController.buscaProdutoInfAdd(nome, tipoM, preco, fornecedor);
-                    if(mercadorias.isEmpty()){
-                         Resposta resultado = new Resposta(302, "Mercadoria não encontrada!");
-                         out.print(resultado.toJSON());
-                    }else{
+                //chamada de método dentro do else
+            } else {
+                if (ativo.equals("true")) {
+                    if (tipoM == null || precoM == null || fornecedor == null) {
+                        Resposta resultado = new Resposta(302, "O parâmetros (tipo,preco e fornecedor) não podem ser nulos");
+                        out.print(resultado.toJSON());
+                    } else {
+                        double preco = Double.parseDouble(precoM);
+                        mercadorias = ProdutoController.buscaProdutoInfAdd(nome, tipoM, preco, fornecedor);
+                        if (mercadorias.isEmpty()) {
+                            Resposta resultado = new Resposta(200, "Mercadoria não encontrada!");
+                            out.print(resultado.toJSON());
+                        } else {
+                            Resposta saida = new Resposta(200, mercadorias.toArray());
+                            out.print(saida.toJSON());
+                        }
                         //exibir na tela web as mercadorias encontradas
                     }
-               //continuar aqui
-               nomeSelecao = "";
-               for(int i =0;i<mercadorias.size();i++){
-                   if(mercadorias.get(i).getNome().equals(nomeSelecao)){
-                       Produto exibicaoMerc = new Produto();
-                       exibicaoMerc = mercadorias.get(i);//variável global?
-                       //mandar para a página web as informações a serem exibidas.
-                   }
-               }
-               }
-           }catch(NumberFormatException ex){
-               Resposta resultado = new Resposta(302, "O parâmetro (nome) não pode conter números.");
-               out.print(resultado.toJSON());
-           }
-       }
+                }
+            }
+        }
+    }
+
+    private void exibicaoMerc(HttpServletRequest request, PrintWriter out) {
+        /*nomeSelecao = "";
+        for (int i = 0; i < mercadorias.size(); i++) {
+            if (mercadorias.get(i).getNome().equals(nomeSelecao)) {
+                Produto exibicaoMerc = mercadorias.get(i);//variável global?
+                //mandar para a página web as informações a serem exibidas.
+            }
+        }*/
+
     }
 }
